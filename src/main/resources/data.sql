3e9bbd5df03de954756191cc897929133e130fb2
DEEPTHI2107 <54965464+DEEPTHI2107@users.noreply.github.com>
2022-09-16 20:15:26 +0530

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into user (userid,upassword,uname) values ('admin','admin','admin');
+
+
+insert into user (userid,upassword,uname) values ('anagha','anagha','anagha');
+insert into user (userid,upassword,uname) values ('deepthi','deepthi','deepthi');
+insert into user (userid,upassword,uname) values ('user3','user3','user3');
+insert into user (userid,upassword,uname) values ('guru','guru','guru');
